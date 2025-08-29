6315593aaf5a7b01b0620cebba01a1dc6511bdda
Arter Tendean <arter@samb.co.id>
2025-08-29 11:13:04 +0700

    Adding tests

--- api_logs/admin.py
+++ api_logs/admin.py
@@ -53,3 +53,12 @@ class APIRequestLogAdmin(ModelAdmin):
             },
         ),
     ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
