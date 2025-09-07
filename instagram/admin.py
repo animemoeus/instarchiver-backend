debd3aec92744afbcbe121388b2a86bce7f6e0d3
Arter Tendean <arter.tendean.07@gmail.com>
2025-09-07 12:02:55 +0700

    Add admin actions to refresh user from API

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from simple_history.admin import SimpleHistoryAdmin
 from unfold.admin import ModelAdmin
+from unfold.decorators import action
 
 from .models import User
 
 
 @admin.register(User)
 class InstagramUserAdmin(SimpleHistoryAdmin, ModelAdmin):
+    actions_detail = ["update_from_api"]
     list_display = [
         "username",
         "full_name",
@@ -71,3 +78,25 @@ class InstagramUserAdmin(SimpleHistoryAdmin, ModelAdmin):
         ),
     )
     ordering = ["-created_at"]
+
+    @action(
+        description=_("Update from Instagram API"),
+        url_path="update-from-api",
+        permissions=["change"],
+    )
+    def update_from_api(self, request: HttpRequest, object_id: str):
+        """Update user profile data from Instagram API."""
+        try:
+            user = User.objects.get(pk=object_id)
+            user.update_profile_from_api()
+            messages.success(
+                request,
+                f"Successfully updated {user.username} from Instagram API.",
+            )
+        except Exception as e:  # noqa: BLE001
+            messages.error(
+                request,
+                "Failed to update user from API: %s" % str(e),  # noqa: UP031
+            )
+
+        return redirect(reverse("admin:instagram_user_change", args=(object_id,)))
