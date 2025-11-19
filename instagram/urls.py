5472d4be8ca12c1770993f67e57e7b5f05a7e879
Arter Tendean <arter.tendean.07@gmail.com>
2025-11-19 16:29:30 +0700

    User Detail API Endpoint (#86)
    * Adding user list API endpoint
    
    * Clean the code
    
    * Adding instagram user factory
    
    * Add comprehensive tests for InstagramUserListView endpoint
    
    * feat: Add user detail endpoint, enhance its serializer with new fields and exclusions, and apply authentication permissions.
    
    * test: add comprehensive tests for InstagramUserDetailView

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
+from instagram.views import InstagramUserDetailView
 from instagram.views import InstagramUserListView
 from instagram.views import ProcessInstagramDataView
 
 app_name = "instagram"
 urlpatterns = [
     path("users/", InstagramUserListView.as_view(), name="user_list"),
+    path("users/<uuid:uuid>/", InstagramUserDetailView.as_view(), name="user_detail"),
     path("inject-data/", ProcessInstagramDataView.as_view(), name="process_data"),
 ]
--- instagram/views/__init__.py
+++ instagram/views/__init__.py
@@ -1,7 +1,9 @@
 from .others import ProcessInstagramDataView
+from .users import InstagramUserDetailView
 from .users import InstagramUserListView
 
 __all__ = [
+    "InstagramUserDetailView",
     "InstagramUserListView",
     "ProcessInstagramDataView",
 ]
