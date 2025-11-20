e0ae4f5d474d6090a56ba27f161f6c1b83c04d30
Arter Tendean <arter.tendean.07@gmail.com>
2025-11-20 12:21:40 +0700

    feat: prefetch user data with `has_stories` and `has_history` annotations for stories list.

--- instagram/views/stories.py
+++ instagram/views/stories.py
@@ -1,7 +1,11 @@
+from django.db.models import Exists
+from django.db.models import OuterRef
+from django.db.models import Prefetch
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from instagram.models import Story
+from instagram.models import User as InstagramUser
 from instagram.paginations import StoryCursorPagination
 from instagram.serializers.stories import StoryListSerializer
 
@@ -13,4 +17,18 @@ class StoryListView(ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        return Story.objects.all().select_related("user").order_by("-created_at")
+        # Annotate users with has_stories and has_history
+        annotated_users = InstagramUser.objects.annotate(
+            has_stories=Exists(Story.objects.filter(user=OuterRef("pk"))),
+            has_history=Exists(
+                InstagramUser.history.model.objects.filter(uuid=OuterRef("pk")),
+            ),
+        )
+
+        return (
+            Story.objects.all()
+            .prefetch_related(
+                Prefetch("user", queryset=annotated_users),
+            )
+            .order_by("-created_at")
+        )
