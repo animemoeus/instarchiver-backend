d91797cff07ae4c1755f8c4a9530290df8d26821
Arter Tendean <arter.tendean.07@gmail.com>
2025-12-16 20:29:59 +0700

    feat: Refactor PostMedia admin inline to StackedInline, move media URLs to readonly fields, and replace Stripe payment script with Instagram post processing.

--- instagram/admin/post.py
+++ instagram/admin/post.py
@@ -1,20 +1,18 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 from unfold.admin import ModelAdmin
-from unfold.admin import TabularInline
+from unfold.admin import StackedInline
 
 from instagram.models import Post
 from instagram.models import PostMedia
 
 
-class PostMediaInline(TabularInline):
+class PostMediaInline(StackedInline):
     """Inline admin for PostMedia."""
 
     model = PostMedia
     extra = 0
     fields = [
-        "thumbnail_url",
-        "media_url",
         "thumbnail",
         "media",
     ]
@@ -22,6 +20,7 @@ class PostMediaInline(TabularInline):
         "thumbnail_url",
         "media_url",
     ]
+    tab = True
 
 
 @admin.register(Post)
